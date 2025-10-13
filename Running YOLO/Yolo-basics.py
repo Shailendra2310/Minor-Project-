ef346667d07044b7ac40cea7105c261d278c3764
Shailendra <Shailendrach1301@gmail.com>
2025-10-13 09:15:30 +0530

    Updated project , Webcam Running with Yolo

--- Running YOLO/Yolo-basics.py
+++ Running YOLO/Yolo-basics.py
@@ -2,5 +2,6 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('yolov8n.pt')
-results = model("Images/friends.jpg", save=True)
+results = model("Images/3.jpg", save=True)
+results = model("Images/2.jpg", save=True)
 cv2.waitKey(0)
